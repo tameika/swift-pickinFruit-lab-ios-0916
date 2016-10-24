cebd04bf2a7795699a802d6ec7dc9e79efea8570
Tameika Lawrence <tameikalawrence@Tameikas-MacBook-Air.local>
2016-10-24 18:53:48 -0400

    Fruit Picked

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var fruitPicker: UIPickerView!
+    
     
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
     
@@ -19,16 +21,59 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
-        
     }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return fruitsArray.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return fruitsArray[row]
+
+    }
+    
+    
+    
+    
     
     @IBAction func spinButtonTapped(_ sender: UIButton) {
         
         
+        // pick random fruit for each component in the picker view
+        for component in 0..<fruitPicker.numberOfComponents {
+            
+            let randomRow = Int(arc4random_uniform(UInt32(fruitsArray.count)))
+            fruitPicker.selectRow(randomRow, inComponent: component, animated: true)
+            
+        }
+        
+        // check to see what fruit was picked at each component in the picker view
+        
+                
+        let selectedFruitComponent0 = fruitsArray[fruitPicker.selectedRow(inComponent: 0)]
+        let selectedFruitComponent1 = fruitsArray[fruitPicker.selectedRow(inComponent: 1)]
+        let selectedFruitComponent2 = fruitsArray[fruitPicker.selectedRow(inComponent: 2)]
+        
+        if (selectedFruitComponent0 == selectedFruitComponent1) && (selectedFruitComponent1 == selectedFruitComponent2) {
+            
+            resultLabel.text = "WINNER!"
+            
+        } else {
+            
+            resultLabel.text = "TRY AGAIN"
+        }
         
     }
-
+    
+    
 }
+
 
 // MARK: Set Up
 extension ViewController {
